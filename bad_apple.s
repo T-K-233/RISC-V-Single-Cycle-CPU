0b0c4ed7041b877ad85f735abbfc1649752dda66
-T.K.- <tk_account@126.com>
2020-08-31 16:01:27 +0800

    FIX load & store addr error

--- bad_apple.s
+++ bad_apple.s
@@ -4,22 +4,22 @@ addi s11, x0, 0
 loop:
 
 lw s0, 0(s11)
-lw s1, 1(s11)
-lw s2, 2(s11)
-lw s3, 3(s11)
-lw s4, 4(s11)
-lw s5, 5(s11)
-lw s6, 6(s11)
-lw s7, 7(s11)
+lw s1, 4(s11)
+lw s2, 8(s11)
+lw s3, 12(s11)
+lw s4, 16(s11)
+lw s5, 20(s11)
+lw s6, 24(s11)
+lw s7, 28(s11)
 
 sw s0, 0(gp)
-sw s1, 1(gp)
-sw s2, 2(gp)
-sw s3, 3(gp)
-sw s4, 4(gp)
-sw s5, 5(gp)
-sw s6, 6(gp)
-sw s7, 7(gp)
+sw s1, 4(gp)
+sw s2, 8(gp)
+sw s3, 12(gp)
+sw s4, 16(gp)
+sw s5, 20(gp)
+sw s6, 24(gp)
+sw s7, 28(gp)
 
 addi s11, s11, 8
 
